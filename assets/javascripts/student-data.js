e3a06294f21a7c93ba0e6f7d5be353b3754074ed
winnab <winnab@gmail.com>
2014-01-20 17:01:55 +0000

    updated data js file with relevant data. created separate instructor data file.

--- assets/javascripts/student-data.js
+++ assets/javascripts/student-data.js
@@ -1,122 +1,122 @@
 var studentData = {
   adam: {
-    name: "Adam",
-    description: "Foo bar",
-    linkedin: "http://uk.linkedin.com/1",
-    github: "https://github.com/1",
-    twitter: "http://twitter.com/1",
-    site: "http://example.com/1"
-  },
-  alexh: {
-    name: "Alex",
-    description: "Bar foo",
-    linkedin: "http://uk.linkedin.com/2",
-    github: "https://github.com/2",
-    twitter: "http://twitter.com/2",
-    site: "http://example.com/2"
-  },
-  maloney: {
-    name: "Maloney",
-    description: "FOO BAR",
-    linkedin: "http://uk.linkedin.com/3",
-    github: "https://github.com/3",
-    twitter: "http://twitter.com/3",
-    site: "http://example.com/3"
-  },
-  jonny: {
-    name: "",
-    description: "",
-    linkedin: "",
-    github: "",
-    twitter: "",
-    site: ""
-  },
-  jack: {
-    name: "",
-    description: "",
-    linkedin: "",
-    github: "",
-    twitter: "",
-    site: ""
-  },
-  jamie: {
-    name: "",
-    description: "",
-    linkedin: "",
-    github: "",
+    name: "Adam Buchan",
+    description: "Venture capitalist turned entrepreneur",
+    linkedin: "http://uk.linkedin.com/in/adambuchan/",
+    github: "http://github.com/AdamBuchan",
     twitter: "",
     site: ""
   },
   alexf: {
-    name: "",
-    description: "",
-    linkedin: "",
-    github: "",
+    name: "Adebamigbe Fasanmade",
+    description: "I am a technology enthusiast. GA has added to my skills as a developer and has given me a chance to work with people from diverse backgrounds.",
+    linkedin: "http://www.linkedin.com/pub/adebamigbe-fasanmade/30/764/739",
+    github: "https://github.com/Adebamigbe",
     twitter: "",
+    site: ""
+  },
+  alexh: {
+    name: "Alex Hamlin",
+    description: "Experienced management consultant meets coder, ultimate ying and yang?",
+    linkedin: "https://uk.linkedin.com/pub/alex-hamlin/5/415/90b/",
+    github: "https://github.com/TheVitruvian",
+    twitter: "https://twitter.com/Londonar",
+    site: "https://www.hamlino.com"
+  },
+  maloney: {
+    name: "Maloney Liu",
+    description: "Bostonified geek from Hong Kong with digital/creative and startup experience. Writes words and code, builds brands and products.",
+    linkedin: "https://uk.linkedin.com/in/maloneyliu",
+    github: "http://github.com/maloneyl",
+    twitter: "https://twitter.com/thinkmaloney",
+    site: "http://www.thinkmaloney.com"
+  },
+  jonny: {
+    name: "Jonny Adshead",
+    description: "Sound engineer turned web developer.",
+    linkedin: "http://www.linkedin.com/pub/jonathan-adshead/7b/b5/779",
+    github: "https://github.com/jadshead",
+    twitter: "https://twitter.com/JonnyAdshead",
+    site: ""
+  },
+  jack: {
+    name: "Jack Lalley",
+    description: "A UI/UX Designer, Jack finally decided he should trade in PSDs for the real thing.",
+    linkedin: "http://www.linkedin.com/pub/jack-lalley/86/317/399",
+    github: "https://github.com/jckly",
+    twitter: "https://twitter.com/jack_lalley",
+    site: ""
+  },
+  jamie: {
+    name: "Jamie Wallace",
+    description: "Founder of walkit.com. Wants to make the world a better place through tech. Now knows more about this coding stuff.",
+    linkedin: "https://uk.linkedin.com/pub/jamie-wallace/2/766/453/",
+    github: "https://github.com/jamiewallace",
+    twitter: "https://twitter.com/jamiewallace",
     site: ""
   },
   daida: {
-    name: "",
-    description: "",
-    linkedin: "",
-    github: "",
-    twitter: "",
+    name: "Daida Medin",
+    description: "Don't ask me. I'm just a girl!",
+    linkedin: "http://www.linkedin.com/in/daidamedina",
+    github: "https://github.com/dmesu",
+    twitter: "https://twitter.com/DaiMedina",
     site: ""
   },
   selina: {
-    name: "",
+    name: "Selina Chotai",
     description: "",
-    linkedin: "",
-    github: "",
-    twitter: "",
+    linkedin: "http://www.linkedin.com/pub/selina-chotai/16/152/317",
+    github: "https://github.com/SelinaC/",
+    twitter: "https://twitter.com/selinachotai",
     site: ""
   },
   sophie: {
-    name: "",
-    description: "",
-    linkedin: "",
-    github: "",
-    twitter: "",
-    site: ""
+    name: "Sophie Chitty",
+    description: "Sophie decided to stop 'wasting' her time in the recycling world in order to 'develop' her interest in coding for the web. A fan of bad word humour.",
+    linkedin: "http://www.linkedin.com/in/sophiechitty",
+    github: "https://github.com/SophGits",
+    twitter: "https://twitter.com/SophTwits",
+    site: "http://sophtumbls.tumblr.com/"
   },
   winna: {
-    name: "",
-    description: "",
-    linkedin: "",
-    github: "",
-    twitter: "",
-    site: ""
+    name: "Winna Bridgewater",
+    description: "Designer and developer",
+    linkedin: "http://www.linkedin.com/in/winnab",
+    github: "https://github.com/winnab",
+    twitter: "http://www.twitter.com/thepreviewmode",
+    site: "http://www.thepreviewmode.com"
   },
   ralph: {
-    name: "",
-    description: "",
-    linkedin: "",
-    github: "",
-    twitter: "",
+    name: "Ralph Reid",
+    description: "A Londoner via Venezuela and is a born Chicagoan. Loves technology, creative problem solving & all things Reggae.",
+    linkedin: "http://uk.linkedin.com/in/reidralph/",
+    github: "https://github.com/ralphreid",
+    twitter: "https://twitter.com/juniorreidirie",
     site: ""
   },
   sharif: {
-    name: "",
-    description: "",
-    linkedin: "",
-    github: "",
-    twitter: "",
+    name: "Sharif Zu'bi",
+    description: "Living the dream, one day at a time!",
+    linkedin: "http://www.linkedin.com/pub/sharif-zu-bi/35/850/2a0",
+    github: "https://github.com/sharifzubi",
+    twitter: "https://twitter.com/sharifzubi",
     site: ""
   },
   neha: {
-    name: "",
-    description: "",
-    linkedin: "",
-    github: "",
-    twitter: "",
-    site: ""
+    name: "Neha Shah",
+    description: "Neha is a lover of tech, owls, books, and chair-o-planes. Prior to WDI, she did marketing and comm for tech companies and startups.",
+    linkedin: "http://www.linkedin.com/in/NehaShah1 ",
+    github: "https://github.com/owlbytes",
+    twitter: "https://twitter.com/neha_shah",
+    site: "http://nehashah.info/"
   },
   oli: {
-    name: "",
-    description: "",
-    linkedin: "",
-    github: "",
-    twitter: "",
-    site: ""
+    name: "Oliver Peate",
+    description: "Should make a Chrome extension to generate intriguing short bios. Until then, lets say Oli loves web development and technology. He claims to know which end of an e-commerce retailer makes profit.",
+    linkedin: "http://www.linkedin.com/in/oliverpeate",
+    github: "https://github.com/odlp",
+    twitter: "https://twitter.com/olipeate",
+    site: "http://oliverpeate.com/"
   }
 }
