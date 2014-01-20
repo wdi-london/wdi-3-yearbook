b1d9636220a6b7aef5d3a9594f39dbafff8e35d4
odlp <oliverp@gmail.com>
2014-01-20 15:30:53 +0000

    basic student switched functionality

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,3 +1,40 @@
+function swapStudent() {
+  if ($(this).hasClass('selected')) {
+    // already selected
+  } else {
+    // update student displayed
+    $studentIcons.filter('.selected').removeClass('selected'); // remove selected class from previous student
+    $(this).addClass('selected');
+    updateStudentProfile(this);
+  }
+};
+
+function updateStudentProfile(ele) {
+  var $studentThumb = $(ele);
+  var studentID     = $studentThumb.attr('data-id');
+  var studentInfo   = studentData[studentID];
+
+  $studentPhoto.attr('src', $studentThumb.attr('src'));
+  $studentName.text(studentInfo.name);
+  $studentDesc.text(studentInfo.description);
+  $studentLinkedin.attr('href', studentInfo.linkedin);
+  $studentGithub.attr('href', studentInfo.github);
+  $studentTwitter.attr('href', studentInfo.twitter);
+  $studentSite.attr('href', studentInfo.site);
+}
+
 $(function() {
-  console.log('hello world');
+  // cache element selections for re-use:
+  $studentIcons     = $('.student-icon');
+  $studentProfile   = $('#student-profile');
+  $studentName      = $studentProfile.find('.student-name');
+  $studentDesc      = $studentProfile.find('.student-desc');
+  $studentPhoto     = $studentProfile.find('.student-photo');
+  $studentLinkedin  = $studentProfile.find('.student-linkedin');
+  $studentGithub    = $studentProfile.find('.student-github');
+  $studentTwitter   = $studentProfile.find('.student-twitter');
+  $studentSite      = $studentProfile.find('.student-site');
+
+  // student icon click event
+  $studentIcons.on('click', swapStudent);
 });
