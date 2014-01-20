d9d5fd2d02be0fdaadfc783e9507402691ec7322
odlp <oliverp@gmail.com>
2014-01-20 15:07:52 +0000

    added skeleton for student data

---
+++ assets/javascripts/student-data.js
@@ -0,0 +1,122 @@
+var studentData = {
+  adam: {
+    name: "Adam",
+    description: "Foo bar",
+    linkedin: "http://uk.linkedin.com/1",
+    github: "https://github.com/1",
+    twitter: "http://twitter.com/1",
+    site: "http://example.com/1"
+  },
+  alexh: {
+    name: "Alex",
+    description: "Bar foo",
+    linkedin: "http://uk.linkedin.com/2",
+    github: "https://github.com/2",
+    twitter: "http://twitter.com/2",
+    site: "http://example.com/2"
+  },
+  maloney: {
+    name: "Maloney",
+    description: "FOO BAR",
+    linkedin: "http://uk.linkedin.com/3",
+    github: "https://github.com/3",
+    twitter: "http://twitter.com/3",
+    site: "http://example.com/3"
+  },
+  jonny: {
+    name: "",
+    description: "",
+    linkedin: "",
+    github: "",
+    twitter: "",
+    site: ""
+  },
+  jack: {
+    name: "",
+    description: "",
+    linkedin: "",
+    github: "",
+    twitter: "",
+    site: ""
+  },
+  jamie: {
+    name: "",
+    description: "",
+    linkedin: "",
+    github: "",
+    twitter: "",
+    site: ""
+  },
+  alexf: {
+    name: "",
+    description: "",
+    linkedin: "",
+    github: "",
+    twitter: "",
+    site: ""
+  },
+  daida: {
+    name: "",
+    description: "",
+    linkedin: "",
+    github: "",
+    twitter: "",
+    site: ""
+  },
+  selina: {
+    name: "",
+    description: "",
+    linkedin: "",
+    github: "",
+    twitter: "",
+    site: ""
+  },
+  sophie: {
+    name: "",
+    description: "",
+    linkedin: "",
+    github: "",
+    twitter: "",
+    site: ""
+  },
+  winna: {
+    name: "",
+    description: "",
+    linkedin: "",
+    github: "",
+    twitter: "",
+    site: ""
+  },
+  ralph: {
+    name: "",
+    description: "",
+    linkedin: "",
+    github: "",
+    twitter: "",
+    site: ""
+  },
+  sharif: {
+    name: "",
+    description: "",
+    linkedin: "",
+    github: "",
+    twitter: "",
+    site: ""
+  },
+  neha: {
+    name: "",
+    description: "",
+    linkedin: "",
+    github: "",
+    twitter: "",
+    site: ""
+  },
+  oli: {
+    name: "",
+    description: "",
+    linkedin: "",
+    github: "",
+    twitter: "",
+    site: ""
+  }
+}
